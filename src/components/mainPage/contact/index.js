c9eb4ce59b29d4197a87c79138a733abc642f38b
gabeleonn <gableonn@gmail.com>
2020-08-03 14:25:12 -0300

    Feat: Contact form added to Contact.

--- src/components/mainPage/contact/index.js
+++ src/components/mainPage/contact/index.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './form.scss';
 
-const index = () => {
+const Index = () => {
+  const [formToggle, setFormToggle] = useState(false);
+
+  const handleForm = () => {
+    setFormToggle(!formToggle);
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setFormToggle(false);
+    console.log("Email sent");
+  };
+
   return (
     <div className="main--resume__contact card">
-      Contact
+      <p className="cto">Get in touch!</p>
+      <button onClick={handleForm} className="button">Send Email</button>
+      {formToggle ?
+        (<div className="form--blur">
+          <form className="form">
+            <h2 className="title">Get in touch</h2>
+            <input type="text" placeholder="Name/Company" />
+            <input type="email" placeholder="Email" />
+            <textarea placeholder="Message"></textarea>
+            <button className="button" onClick={e => sendEmail(e)}>Send Message</button>
+          </form>
+        </div>) : ""}
     </div>
   );
 };
 
-export default index;
+export default Index;
--- src/components/mainPage/header/SwitchButton.js
+++ src/components/mainPage/header/SwitchButton.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'switchButton.scss';
 
 const SwitchButton = () => {
   return (
