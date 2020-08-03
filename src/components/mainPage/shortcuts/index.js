08cdcfcefb0c59b8352ee72fab6f00cb0b8b564c
gabeleonn <gableonn@gmail.com>
2020-08-03 13:26:10 -0300

    Feat: Realocated SwitchButton to Header.

--- src/components/mainPage/shortcuts/index.js
+++ src/components/mainPage/shortcuts/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import SwitchButton from './SwitchButton';
 
 const index = () => {
   return (
     <div className="main--resume__shortcuts card">
       Shortcuts<br />
-      PT<SwitchButton />EN
     </div>
   );
 };
