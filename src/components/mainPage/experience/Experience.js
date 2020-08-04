0bb51658dc62dc503e617f214aa708e2981828ff
gabeleonn <gableonn@gmail.com>
2020-08-04 20:04:20 -0300

    Feat: Experience list added.

--- src/components/mainPage/experience/Experience.js
+++ src/components/mainPage/experience/Experience.js
@@ -1,11 +1,27 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import './experience.scss';
 
 const Experience = ({ experience }) => {
-  return (
-    <div className="experience">
+  const {
+    isActual, company, jobTitle, city, state, since, to, time, website,
+  } = experience;
 
-    </div>
+  return (
+    <li className={isActual ? 'active' : ''}>
+      <div className="experience__item">
+        <div className="left">
+          <p className="job-title">{jobTitle}</p>
+          <p className="city">{city}, {state}</p>
+          <a href={website} target="_blank" rel="noopener noreferrer" className="company">{company}</a>
+        </div>
+        <div className="right">
+          <p className="time">{time}</p>
+          <p className="since">{since} - {to}</p>
+        </div>
+      </div>
+      <div className="border--bottom" />
+    </li>
   );
 };
 
--- src/components/mainPage/experience/index.js
+++ src/components/mainPage/experience/index.js
@@ -1,38 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Experience from './Experience';
 
 const index = () => {
-
   const experiences = [
     {
       isActual: true,
-      company: "Company Name",
-      jobTitle: "Job Title",
-      city: "City",
-      state: "ST",
-      since: "April 2000",
-      to: "June 2008",
-      time: "8 years and 2 months",
-      website: "http://google.com"
+      company: 'Company Name',
+      jobTitle: 'Job Title',
+      city: 'City',
+      state: 'ST',
+      since: 'April 2000',
+      to: 'June 2008',
+      time: '8 years and 2 months',
+      website: 'http://google.com',
     },
     {
       isActual: false,
-      company: "Company Name 1",
-      jobTitle: "Job Title",
-      city: "City",
-      state: "ST",
-      since: "April 2000",
-      to: "June 2008",
-      time: "8 years and 2 months",
-      website: "http://google.com"
-    }
+      company: 'Company Name',
+      jobTitle: 'Job Title',
+      city: 'City',
+      state: 'ST',
+      since: 'April 2000',
+      to: 'June 2008',
+      time: '8 years and 2 months',
+      website: 'http://google.com',
+    },
+    {
+      isActual: false,
+      company: 'Company Name 1',
+      jobTitle: 'Job Title',
+      city: 'City',
+      state: 'ST',
+      since: 'April 2000',
+      to: 'June 2008',
+      time: '8 years and 2 months',
+      website: 'http://google.com',
+    },
   ];
 
   return (
     <div className="main--resume__experience card">
       <h3 className="card__title">Experience</h3>
-      <div className="border--bottom"></div>
-      {experiences.map(experience => <Experience key={experience.company} experience={experience} />)}
+      <div className="border--bottom" />
+      <ul>
+        {experiences.map((e) => <Experience key={e.company} experience={e} />)}
+      </ul>
+      <Link to="/" className="link">See more</Link>
     </div>
   );
 };
